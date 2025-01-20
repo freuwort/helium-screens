6e30beaa460db7350489c826611d96d584a5101c
Eltaco-GH <fabian.raddatz@aol.de>
2025-01-20 18:52:46 +0100

    Alte Poster entfernt

--- screen.js
+++ screen.js
@@ -5,13 +5,12 @@ export default {
     horizontal_resolution: 1080,
     vertical_resolution: 1920,
     view_order: [
-        1,2,3,4,5,6,7,8,15,16,17,18,19,20,21,22,23,24
+        1,2,3,5,6,7,8,15,16,17,18,19,20,21,24
     ],
     views: [
         {id:  1, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/trabs_1.jpg' }]},
         {id:  2, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/trabs_2.jpg' }]},
         {id:  3, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/trabs_3.jpg' }]},
-        {id:  4, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/kids_play_date.jpg' }]},
         {id:  5, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/neues_dach.jpg' }]},
         {id:  6, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/wlan.jpg' }]},
         {id:  7, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/aufruf_kneipenabend.jpg' }]},
@@ -23,8 +22,6 @@ export default {
         {id: 19, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/puppy_2_neu.jpg' }]},
         {id: 20, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/travent_1.png' }]},
         {id: 21, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/travent_2.png' }]},
-        {id: 22, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/queermonics_1.png' }]},
-        {id: 23, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/wm_dance.jpg' }]},
         {id: 24, on_days: [0, 5, 6], components: [{ background: '/images/poster/kft25.png' }]},
     ],
 }
