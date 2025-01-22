b7e6fd075a246db5f9f267651ae832b0a181f8eb
Eltaco-GH <fabian.raddatz@aol.de>
2025-01-22 17:25:31 +0100

    Fehler korrigiert

--- screen.js
+++ screen.js
@@ -5,25 +5,24 @@ export default {
     horizontal_resolution: 1080,
     vertical_resolution: 1920,
     view_order: [
-        1, 9, 7, 6, 2, 10, 8, 12, 3, 13, 14, 7, 11, 6, 15,  16, 17, 18
+        1, 9, 7, 6, 2, 10, 8, 12, 3, 13, 14, 7, 11, 6, 15,  16, 17,
     ],
     views: [
         {id:  1, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/trabs_1.jpg' }]},
         {id:  2, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/trabs_2.jpg' }]},
-        {id:  3, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/trabs_3.jpg' }]},
-           
+        {id:  3, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/trabs_3.jpg' }]},     
         {id:  6, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/wlan.jpg' }]},
         {id:  7, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/aufruf_kneipenabend.jpg' }]},
         {id:  8, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/aufruf_plakate.jpg' }]},
-        {id:  9, on_days: [1, 4, 0, 5], components: [{ background: '/images/poster/jtl_1.jpg' }]},
-        {id: 10, on_days: [2, 5], components: [{ background: '/images/poster/jtl_2.jpg' }]},
+        {id:  9, on_days: [1, 4, 0, 5, 6], components: [{ background: '/images/poster/jtl_1.jpg' }]},
+        {id: 10, on_days: [2, 5, 6], components: [{ background: '/images/poster/jtl_2.jpg' }]},
         {id: 11, on_days: [3, 6, 5], components: [{ background: '/images/poster/jtl_3.jpg' }]},
-        {id: 12, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/BTWOW1.jpg' }]},
-        {id: 13, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/BTWOW2.jpg' }]},
-        {id: 14, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/BTWOW3.jpg' }] },
-        {id: 15, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/BTWOW4.jpg' }] },
-        {id: 16, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/BTWOW5.jpg' }] },
+        {id: 12, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/BTWOW1.png' }]},
+        {id: 13, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/BTWOW2.png' }]},
+        {id: 14, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/BTWOW3.png' }] },
+        {id: 15, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/BTWOW4.png' }] },
+        {id: 16, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/BTWOW5.png' }] },
         {id: 17, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/puppy_1.jpg' }] },
-        {id: 18, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/puppy_2_neu.jpg' }] },
+        
     ],
 }
