e15eb0b9ad1bc98b16223bde2a06147f383a144b
Eltaco-GH <fabian.raddatz@aol.de>
2025-02-27 20:28:22 +0100

    Altes Entfernt

--- screen.js
+++ screen.js
@@ -11,22 +11,14 @@ export default {
         {id:  1, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/trabs_1.jpg' }]},
         {id:  2, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/trabs_2.jpg' }]},
         {id:  3, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/trabs_3.jpg' }]},
-            
         {id:  6, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/wlan.jpg' }]},
         {id:  7, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/aufruf_kneipenabend.jpg' }]},
         {id:  8, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/aufruf_plakate.jpg' }]},
         {id:  9, on_days: [1, 4, 0, 5, 6], components: [{ background: '/images/poster/jtl_1.jpg' }]},
         {id: 10, on_days: [2, 5, 6], components: [{ background: '/images/poster/jtl_2.jpg' }]},
         {id: 11, on_days: [3, 6, 5], components: [{ background: '/images/poster/jtl_3.jpg' }]},
-        {id: 12, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/BTWOW1.png' }]},
-        {id: 13, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/BTWOW2.png' }]},
-        {id: 14, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/BTWOW3.png' }] },
-        {id: 15, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/BTWOW4.png' }] },
-        {id: 16, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/BTWOW5.png' }] },
         {id: 17, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/puppy_1.jpg' }] },
-       
         {id: 19, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/FF_Jahresprogramm.png' }]},
-        
         {id: 21, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/MWD.jpg' }] },
         {id: 30, on_days: [0, 1, 2, 3, 4, 5, 6], components: [{ background: '/images/poster/Schwarz.jpg' }] },
     ],
